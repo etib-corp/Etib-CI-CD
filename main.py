dab7df461ce1c4e08e50d04b09be08fbf7de708a
Nathan <nathan.maillot@epitech.eu>
2023-11-26 02:12:18 +0100

    feat: add error messages with color and flags

---
+++ main.py
@@ -0,0 +1,42 @@
+from src.TestFileChecker import TestFileChecker
+import sys
+from colorama import Fore, Back
+
+def fillFlags(args):
+  flags = {
+    "display": False,
+    "help": False,
+    "report": False,
+  }
+  for arg in args:
+    match arg:
+      case "-h":
+        flags["help"] = True
+      case "--help":
+        flags["help"] = True
+      case "-d":
+        flags["display"] = True
+      case "--display":
+        flags["display"] = True
+      case "-r":
+        flags["report"] = True
+      case "--report":
+        flags["report"] = True
+      case _:
+        print(Back.RED + "ARGUMENT ERROR:" + Back.RESET + Fore.RED + " Invalid Flag.")
+        return None
+
+  if (flags["display"] and flags["help"]) or (flags["report"] and flags["help"]):
+    print(Back.RED + "ARGUMENT ERROR:" + Back.RESET + Fore.RED + " Incompatible Flag use.")
+    return None
+  return flags
+
+def execute():
+  args = sys.argv[1:]
+  flags = fillFlags(args)
+
+  if flags is None:
+    return -1
+  return TestFileChecker(flags)
+
+execute()
--- src/TestFileChecker.py
+++ src/TestFileChecker.py
@@ -1,22 +1,35 @@
 import os
+from colorama import Fore, Back
+
 from src.Parser import getFiles
 from src.Display import printDir
 
-def TestFileChecker():
+def Help():
+  with open('man.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+      print(Fore.BLUE + line)
+
+def TestFileChecker(flags : dict):
+  if flags["help"]:
+    return Help()
   cwd = os.getcwd()
 
   if os.path.exists(cwd + '/tests') == False:
-    print('Test directory does not exist.')
+    print(Back.RED + "STRUCTURE ERROR:" + Back.RESET + Fore.RED + " Test directory does not exist.")
     return -1
   src = getFiles(cwd, '/src')
   tests = getFiles(cwd, '/tests')
 
-  printDir(tests, 0)
-  print("\n\n")
-  printDir(src, 0)
+  if flags["display"]:
+    print(Fore.MAGENTA)
+    printDir(tests, 0)
+    print("\n\n")
+    print(Fore.CYAN)
+    printDir(src, 0)
   if src.compareTo(tests, True):
-    print("OK: All test files exists.")
+    print(Back.GREEN + "OK:" + Back.RESET + Fore.GREEN + " All test files exists.")
   else:
-    print("WARNING: Some tests are missing OR there are too many tests files compared to source code.")
+    print(Back.YELLOW + "WARNING:" + Back.RESET + Fore.YELLOW + " Some tests are missing OR there are too many tests files compared to source code.")
     return -1
   return 0
